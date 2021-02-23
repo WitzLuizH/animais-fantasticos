3e122c03335fd8ea0294f992515b3487d5d9fb23
Luiz Witz <witz.luizh@gmail.com>
2021-02-23 16:41:56 -0300

    Correcoes gerais de bugs

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,37 +1,38 @@
 import outsideClick from "./outsideclick.js";
 
 export default class MenuMobile {
-  constructor(menuButton, menuList, events) {
-    this.menuButton = document.querySelector(menuButton);
-    this.menuList = document.querySelector(menuList);
-    this.activeClass = "active";
+    constructor(menuButton, menuList, events) {
+        this.menuButton = document.querySelector(menuButton);
+        this.menuList = document.querySelector(menuList);
+        this.activeClass = "active";
 
-    // Define touchstart e click como argumento padrao
-    // de eventos caso o usuario nao defina.
-    if (events === undefined) this.events = ["touchstart", "click"];
-    else this.events = events;
+        // Define touchstart e click como argumento padrao
+        // de eventos caso o usuario nao defina.
+        if (events === undefined) this.events = ["touchstart", "click"];
+        else this.events = events;
 
-    this.openMenu = this.openMenu.bind(this);
-  }
-  openMenu() {
-    this.menuList.classList.add(this.activeClass);
-    this.menuButton.classList.add(this.activeClass);
-    outsideClick(this.menuList, this.events, () => {
-      this.menuList.classList.remove(this.activeClass);
-      this.menuButton.classList.remove(this.activeClass);
-    });
-  }
-
-  addMenuMobileEvents() {
-    this.events.forEach((evento) =>
-      this.menuButton.addEventListener(evento, this.openMenu)
-    );
-  }
-
-  init() {
-    if (this.menuButton && this.menuList) {
-      this.addMenuMobileEvents();
+        this.openMenu = this.openMenu.bind(this);
     }
-    return this;
-  }
+    openMenu(event) {
+        event.preventDefault();
+        this.menuList.classList.add(this.activeClass);
+        this.menuButton.classList.add(this.activeClass);
+        outsideClick(this.menuList, this.events, () => {
+            this.menuList.classList.remove(this.activeClass);
+            this.menuButton.classList.remove(this.activeClass);
+        });
+    }
+
+    addMenuMobileEvents() {
+        this.events.forEach((evento) =>
+            this.menuButton.addEventListener(evento, this.openMenu)
+        );
+    }
+
+    init() {
+        if (this.menuButton && this.menuList) {
+            this.addMenuMobileEvents();
+        }
+        return this;
+    }
 }
